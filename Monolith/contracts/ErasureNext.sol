a4c782c6d50a14efc948daa40ccadbeb5fc09ca0
Stephane Gosselin <thegostep@gmail.com>
2019-05-14 15:38:06 -0700

    add mock NMR

--- Monolith/contracts/ErasureNext.sol
+++ Monolith/contracts/ErasureNext.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
-import "./helpers/openzeppelin-eth/math/SafeMath.sol";
-import "./helpers/openzeppelin-eth/token/ERC20/ERC20Burnable.sol";
+import "./helpers/openzeppelin-solidity/math/SafeMath.sol";
+import "./helpers/openzeppelin-solidity/token/ERC20/ERC20Burnable.sol";
 
 // one to many relationship between Post and Agreement
 
