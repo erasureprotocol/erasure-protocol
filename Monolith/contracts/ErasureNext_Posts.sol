099dbbbc35d664930cd613ca776191bea9bf99c0
Stephane Gosselin <thegostep@gmail.com>
2019-06-17 15:03:33 -0700

    remove post symmetric griefing

--- Monolith/contracts/ErasureNext_Posts.sol
+++ Monolith/contracts/ErasureNext_Posts.sol
@@ -17,14 +17,11 @@ contract ErasureNext_Posts is IPFSWrapper {
         IPFSMultiHash[] hashes;
         address owner;
         bytes metadata;
-        uint256 stake;
-        bool symmetricGrief;
     }
 
-    event PostCreated(uint256 postID, address owner, bytes metadata, uint256 stake, bool symmetricGrief);
-    event PostUpdated(uint256 postID, address owner, bytes metadata, uint256 stake, bool symmetricGrief);
+    event PostCreated(uint256 postID, address owner, bytes metadata);
+    event PostUpdated(uint256 postID, address owner, bytes metadata);
     event HashSubmitted(uint256 postID, bytes proofHash);
-    event PostGriefed(uint256 postID, address griefer, uint256 amount, bytes message);
 
     constructor(address _nmr) public {
         nmr = _nmr;
@@ -32,21 +29,17 @@ contract ErasureNext_Posts is IPFSWrapper {
 
     // POSTS //
 
-    function createPost(bytes memory proofHash, bytes memory metadata, uint256 stake, bool symmetricGrief) public returns (uint256 postID) {
+    function createPost(bytes memory proofHash, bytes memory metadata) public returns (uint256 postID) {
 
         postID = posts.length;
         posts.length++;
 
-        require(ERC20Burnable(nmr).transferFrom(msg.sender, address(this), stake));
-
         posts[postID].owner = msg.sender;
         posts[postID].metadata = metadata;
-        posts[postID].stake = stake;
-        posts[postID].symmetricGrief = symmetricGrief;
 
         submitHash(postID, proofHash);
 
-        emit PostCreated(postID, msg.sender, metadata, stake, symmetricGrief);
+        emit PostCreated(postID, msg.sender, metadata);
     }
 
     function submitHash(uint256 postID, bytes memory proofHash) public {
@@ -60,38 +53,16 @@ contract ErasureNext_Posts is IPFSWrapper {
         emit HashSubmitted(postID, proofHash);
     }
 
-    function updatePost(uint256 postID, bytes memory metadata, uint256 stake, bool symmetricGrief) public {
+    function updatePost(uint256 postID, bytes memory metadata) public {
 
         Post storage post = posts[postID];
 
         require(msg.sender == post.owner, "only owner");
 
-        // not vulnerable to re-entrancy since token contract is trusted
-        if (stake > post.stake)
-            require(ERC20Burnable(nmr).transferFrom(msg.sender, address(this), stake - post.stake));
-        if (stake < post.stake)
-            require(ERC20Burnable(nmr).transfer(msg.sender, post.stake - stake));
-
         post.metadata = metadata;
-        post.stake = stake;
-        post.symmetricGrief = symmetricGrief;
 
-        emit PostUpdated(postID, msg.sender, metadata, stake, symmetricGrief);
+        emit PostUpdated(postID, msg.sender, metadata);
     }
 
-    // known to be vulnerable to front-running
-    function griefPost(uint256 postID, uint256 amount, bytes memory message) public {
-
-        Post storage post = posts[postID];
-
-        require(post.symmetricGrief);
-
-        post.stake = post.stake.sub(amount);
-
-        ERC20Burnable(nmr).burn(amount);
-        ERC20Burnable(nmr).burnFrom(msg.sender, amount);
-
-        emit PostGriefed(postID, msg.sender, amount, message);
-    }
 
 }
