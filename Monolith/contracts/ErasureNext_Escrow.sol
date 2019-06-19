89780603befa0b797cceab43c20e6a78911510ad
Stephane Gosselin <thegostep@gmail.com>
2019-06-19 15:04:49 -0700

    remove agreement dependency from escrow

--- Monolith/contracts/ErasureNext_Escrow.sol
+++ Monolith/contracts/ErasureNext_Escrow.sol
@@ -11,68 +11,68 @@ contract ErasureNext_Escrow {
     Escrow[] private escrows;
 
     struct Escrow {
-        address buyer;
-        address seller;
-        uint256 agreementID;
+        address payer;
+        address counterparty;
         address token;
         uint256 amount;
         uint256 deadline;
+        bytes metadata;
     }
 
-    event EscrowCreated(uint256 escrowID, uint256 agreementID, address buyer, address seller);
+    event EscrowCreated(uint256 escrowID, address payer, address counterparty, bytes metadata);
     event FundsSubmitted(uint256 escrowID, address token, uint256 amount, uint256 deadline);
     event DataSubmitted(uint256 escrowID, bytes encryptedData);
     event FundsWithdrawn(uint256 escrowID);
 
     // ESCROWS //
 
-    function createEscrow(uint256 agreementID, address buyer, address seller) public returns (uint256 escrowID) {
+    function createEscrow(address payer, address counterparty, bytes memory metadata) public returns (uint256 escrowID) {
 
         escrowID = escrows.length;
         escrows.length++;
         Escrow storage escrow = escrows[escrowID];
 
-        escrow.buyer = buyer;
-        escrow.seller = seller;
-        escrow.agreementID = agreementID;
+        escrow.payer = payer;
+        escrow.counterparty = counterparty;
+        escrow.metadata = metadata;
 
-        emit EscrowCreated(escrowID, agreementID, buyer, seller);
+        emit EscrowCreated(escrowID, payer, counterparty, metadata);
     }
 
-    function submitFunds(uint256 escrowID, address token, uint256 amount, uint256 duration) public {
+    function submitFunds(uint256 escrowID, address token, uint256 amount, uint256 deadline) public {
 
         Escrow storage escrow = escrows[escrowID];
 
-        require(msg.sender == escrow.buyer);
+        require(msg.sender == escrow.payer);
 
         require(IERC20(token).transferFrom(msg.sender, address(this), amount));
 
         escrow.token = token;
         escrow.amount = amount;
-        escrow.deadline = duration.add(block.timestamp);
+        escrow.deadline = deadline;
 
         emit FundsSubmitted(escrowID, msg.sender, amount, escrow.deadline);
     }
 
-    function submitData(uint256 escrowID, bytes memory encryptedData) public {
+    function submitData(uint256 escrowID, bytes memory data) public {
 
         Escrow storage escrow = escrows[escrowID];
 
-        require(msg.sender == escrow.seller);
+        require(msg.sender == escrow.counterparty);
         require(block.timestamp < escrow.deadline); // This should be checked off-chain because if the tx fails, the data is revealed without a payment
 
         require(IERC20(escrow.token).transfer(msg.sender, escrow.amount));
 
         delete escrow.amount;
 
-        emit DataSubmitted(escrowID, encryptedData);
+        emit DataSubmitted(escrowID, data);
     }
 
     function withdrawFunds(uint256 escrowID) public {
 
         Escrow storage escrow = escrows[escrowID];
 
-        require(msg.sender == escrow.buyer);
+        require(msg.sender == escrow.payer);
         require(block.timestamp >= escrow.deadline);
 
         require(IERC20(escrow.token).transfer(msg.sender, escrow.amount));
@@ -80,6 +80,48 @@ contract ErasureNext_Escrow {
         delete escrow.amount;
 
         emit FundsWithdrawn(escrowID);
+    }
+
+    // Getters //
+
+    function getEscrow(uint256 escrowID) public view returns (
+        address payer,
+        address counterparty,
+        address token,
+        uint256 amount,
+        uint256 deadline,
+        bytes memory metadata
+    ) {
+        payer = escrows[escrowID].payer;
+        counterparty = escrows[escrowID].counterparty;
+        token = escrows[escrowID].token;
+        amount = escrows[escrowID].amount;
+        deadline = escrows[escrowID].deadline;
+        metadata = escrows[escrowID].metadata;
+    }
+
+    function getEscrowPayer(uint256 escrowID) public view returns (address payer) {
+        payer = escrows[escrowID].payer;
+    }
+
+    function getEscrowCounterparty(uint256 escrowID) public view returns (address counterparty) {
+        counterparty = escrows[escrowID].counterparty;
+    }
+
+    function getEscrowToken(uint256 escrowID) public view returns (address token) {
+        token = escrows[escrowID].token;
+    }
+
+    function getEscrowAmount(uint256 escrowID) public view returns (uint256 amount) {
+        amount = escrows[escrowID].amount;
+    }
+
+    function getEscrowDeadline(uint256 escrowID) public view returns (uint256 deadline) {
+        deadline = escrows[escrowID].deadline;
+    }
+
+    function getEscrowMetadata(uint256 escrowID) public view returns (bytes memory metadata) {
+        metadata = escrows[escrowID].metadata;
     }
 
 }
