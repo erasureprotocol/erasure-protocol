3c60e857c8febd77428d57acbaddc676a41356cb
Stephane Gosselin <stephane@numer.ai>
2019-07-22 14:46:02 -0700

    WIP tessellation (#21)

---
+++ Tessellation/contracts/AgreementTemplates/README.md
@@ -0,0 +1,7 @@
+# Agreement Templates
+
+## List of Templates
+
+1. Any counterparty
+2. One counterparty
+3. Multiple counterparty
--- Tessellation/contracts/IFactory.sol
+++ Tessellation/contracts/IFactory.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.5.0;
 
 contract IFactory {
 
-    function getPayloadSize() public returns (uint256 initPayloadSize, uint256 numSignatures);
+    /* function getPayloadSize() external returns (uint256 payloadSize, uint256 numSignatures); */
 
-    function intializeAgreement(bytes memory initPayload, bytes memory signatures) public returns (address agreement);
+    function createInstance(address token, bytes calldata initData) external returns (address instance);
+
+    function getMetadata() external returns (bytes memory metadata);
 
 }
