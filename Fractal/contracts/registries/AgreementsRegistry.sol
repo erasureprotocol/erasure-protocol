15a7c0b6d6ad84e0e7697be61420adf620f6bc99
Stephane Gosselin <thegostep@gmail.com>
2019-07-26 15:06:27 -0700

    increase future proofing

--- Fractal/contracts/registries/AgreementsRegistry.sol
+++ Fractal/contracts/registries/AgreementsRegistry.sol
@@ -4,7 +4,8 @@ import "./Registry.sol";
 
 
 contract AgreementsRegistry is Registry {
-    event AgreementCreated(address agreement, address creator, uint256 index);
+
+    event AgreementCreated(address agreement, address creator, uint256 factoryID);
 
     struct Agreement {
         address agreement;
@@ -15,36 +16,28 @@ contract AgreementsRegistry is Registry {
     // TODO: private with explicit getters
     Agreement[] public agreements;
 
+    constructor() public Registry('Agreement') {
 
-    constructor() public {
-        require(
-            RegistryType(getRegistryType()) == RegistryType.Agreement,
-            "Incorrect registry type is set."
-        );
     }
 
     function append(address agreement, address creator, uint64 extraData) public {
         (
-            bool exists,
-            uint16 index,
-            , // address implementation
-            bool retired,
+            Registry.FactoryState state,
+            uint16 factoryID,
             // bytes memory extraData
         ) = getFactory(msg.sender);
+
+        // ensure that the caller is a registered factory
         require(
-            exists,
-            "Agreement must originate from an approved factory."
-        );
-        require(
-            !retired,
-            "Agreement cannot originate from a retired factory."
+            state == FactoryState.Registered,
+            "Factory in wrong state."
         );
 
         uint256 agreementIndex = agreements.length;
         agreements.push(
             Agreement({
                 agreement: agreement,
-                factoryID: index,
+                factoryID: factoryID,
                 extraData: extraData
             })
         );
--- Fractal/contracts/registries/Registry.sol
+++ Fractal/contracts/registries/Registry.sol
@@ -1,42 +1,28 @@
 pragma solidity ^0.5.0;
 
-import "../helpers/openzeppelin-solidity/introspection/ERC165Checker.sol";
 import "../helpers/openzeppelin-solidity/ownership/Ownable.sol";
 import "../IFactory.sol";
 
 contract Registry is Ownable {
-    using ERC165Checker for address;
 
-    enum RegistryType { Invalid, Agreement, Post, Escrow }
+    enum FactoryState { Unregistered, Registered, Retired }
 
-    event FactoryAdded(address factory, address implementation, uint256 index);
-    event FactoryRetired(address factory, uint256 index);
+    event FactoryAdded(address owner, address factory, uint256 factoryID, bytes extraData);
+    event FactoryRetired(address owner, address factory, uint256 factoryID);
 
     struct Factory {
-        bool exists;
-        uint16 index;
+        FactoryState state;
+        uint16 factoryID;
         address factoryAddress;
-        address implementation;
-        bool retired;
         bytes extraData;
     }
 
-    /*
-     *  bytes4(keccak256('create(bytes)')) == 0xcf5ba53f
-     *  bytes4(keccak256('getInstanceType()')) == 0x18c2f4cf
-     *  bytes4(keccak256('getInstanceRegistry()')) == 0xa5e13904
-     *  bytes4(keccak256('getImplementation()')) == 0xaaf10f42
-     *
-     *  => 0xcf5ba53f ^ 0x18c2f4cf ^ 0xa5e13904 ^ 0xaaf10f42 == 0xd88967b6
-     */ 
-    bytes4 private constant _INTERFACE_ID_IFACTORY = 0xd88967b6;
-
-    RegistryType private _registryType;
+    bytes4 private _registryType;
     address[] private _factoryAddresses;
     mapping(address => Factory) private _factories;
 
-    constructor(RegistryType registryType) public {
-        _registryType = registryType;
+    constructor(string memory registryType) public {
+        _registryType = bytes4(keccak256(bytes(registryType)));
     }
 
     function addFactory(
@@ -45,54 +31,26 @@ contract Registry is Ownable {
     ) external onlyOwner() {
         // get the factory object from storage.
         Factory storage factoryStorage = _factories[factoryAddress];
-        
+
         // ensure that the provided factory is new.
         require(
-            !factoryStorage.exists,
+            factoryStorage.state == FactoryState.Unregistered,
             "Factory already exists at the provided factory address."
         );
 
-        // check the factory interface.
-        require(
-            factoryAddress._supportsInterface(_INTERFACE_ID_IFACTORY),
-            "Factory does not support the required ERC165 interface."
-        );
-        IFactory factoryContract = IFactory(factoryAddress);
-
-        // check the registry address.
-        require(
-            factoryContract.getInstanceRegistry() == address(this),
-            "Factory does not recognize this registry."
-        );
-
-        // check the factory type.
-        require(
-            uint8(factoryContract.getInstanceType()) == uint8(_registryType),
-            "Factory does not have the same type as this registry."
-        );
-
-        // get the implementation address.
-        address implementation = factoryContract.getImplementation();
-        require(
-            implementation != address(0),
-            "Factory does not have an implementation set."
-        );
-
-        // get the index of the new factory.
-        uint16 index = uint16(_factoryAddresses.length);
+        // get the factoryID of the new factory.
+        uint16 factoryID = uint16(_factoryAddresses.length);
 
         // set all of the information for the new factory.
-        factoryStorage.exists = true;
-        factoryStorage.index = index;
+        factoryStorage.state = FactoryState.Registered;
+        factoryStorage.factoryID = factoryID;
         factoryStorage.factoryAddress = factoryAddress;
-        factoryStorage.implementation = implementation;
-        factoryStorage.retired = false;
         factoryStorage.extraData = extraData;
 
         _factoryAddresses.push(factoryAddress);
 
         // emit an event.
-        emit FactoryAdded(factoryAddress, implementation, index);
+        emit FactoryAdded(msg.sender, factoryAddress, factoryID, extraData);
     }
 
     function retireFactory(
@@ -100,45 +58,35 @@ contract Registry is Ownable {
     ) external onlyOwner() {
         // get the factory object from storage.
         Factory storage factoryStorage = _factories[factoryAddress];
-        
+
         // ensure that the provided factory is new and not already retired.
         require(
-            factoryStorage.exists,
-            "Factory does not exist at the provided factory address."
-        );
-        require(
-            !factoryStorage.retired,
-            "Factory has already been retired."
+            factoryStorage.state == FactoryState.Registered,
+            "Factory in wrong state."
         );
 
         // retire the factory.
-        factoryStorage.retired = true;
+        factoryStorage.state = FactoryState.Retired;
 
-        emit FactoryRetired(factoryAddress, factoryStorage.index);
+        emit FactoryRetired(msg.sender, factoryAddress, factoryStorage.factoryID);
     }
 
-    // returning RegistryType here requires `pragma experimental ABIEncoderV2;`
-    function getRegistryType() public view returns (uint8) {
-        return uint8(_registryType);
-    } 
+    function getRegistryType() public view returns (bytes4 registryType) {
+        registryType = _registryType;
+    }
 
     function getFactories() public view returns (address[] memory) {
         return _factoryAddresses;
     }
 
-    // returning Factory here requires `pragma experimental ABIEncoderV2;`
     function getFactory(address factoryAddress) public view returns (
-        bool exists,
-        uint16 index,
-        address implementation,
-        bool retired,
+        FactoryState state,
+        uint16 factoryID,
         bytes memory extraData
     ) {
         Factory memory factory = _factories[factoryAddress];
-        exists = factory.exists;
-        index = factory.index;
-        implementation = factory.implementation;
-        retired = factory.retired;
+        state = factory.state;
+        factoryID = factory.factoryID;
         extraData = factory.extraData;
     }
 }
