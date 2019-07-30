5fb616f48c9c558c8e7c073536dd41359c9ad77e
kenchangh <guanhao3797@gmail.com>
2019-07-30 17:17:27 +0800

    changed indent to 4 spaces

--- Fractal/contracts/test-contracts/TestStaking.sol
+++ Fractal/contracts/test-contracts/TestStaking.sol
@@ -3,27 +3,27 @@ pragma solidity ^0.5.0;
 import "../modules/Staking.sol";
 
 contract TestStaking is Staking {
-  function setToken(address token) public {
-    Staking._setToken(token);
-  }
+    function setToken(address token) public {
+        Staking._setToken(token);
+    }
 
-  function addStake(address staker, address funder, uint256 currentStake, uint256 amountToAdd) public {
-    Staking._addStake(staker, funder, currentStake, amountToAdd);
-  }
+    function addStake(address staker, address funder, uint256 currentStake, uint256 amountToAdd) public {
+        Staking._addStake(staker, funder, currentStake, amountToAdd);
+    }
 
-  function takeStake(address staker, address recipient, uint256 currentStake, uint256 amountToTake) public {
-    Staking._takeStake(staker, recipient, currentStake, amountToTake);
-  }
+    function takeStake(address staker, address recipient, uint256 currentStake, uint256 amountToTake) public {
+        Staking._takeStake(staker, recipient, currentStake, amountToTake);
+    }
 
-  function takeFullStake(address staker, address recipient) public {
-    Staking._takeFullStake(staker, recipient);
-  }
+    function takeFullStake(address staker, address recipient) public {
+        Staking._takeFullStake(staker, recipient);
+    }
 
-  function burnStake(address staker, uint256 currentStake, uint256 amountToBurn) public {
-    Staking._burnStake(staker, currentStake, amountToBurn);
-  }
+    function burnStake(address staker, uint256 currentStake, uint256 amountToBurn) public {
+        Staking._burnStake(staker, currentStake, amountToBurn);
+    }
 
-  function burnFullStake(address staker) public {
-    Staking._burnFullStake(staker);
-  }
+    function burnFullStake(address staker) public {
+        Staking._burnFullStake(staker);
+    }
 }
