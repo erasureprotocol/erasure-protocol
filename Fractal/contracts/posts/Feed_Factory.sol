653df9349920a2cb45509a020707e33b90f674bd
Stephane Gosselin <thegostep@gmail.com>
2019-08-02 12:12:53 -0700

    implement Feed Factory fixes #75

--- Fractal/contracts/posts/Feed_Factory.sol
+++ Fractal/contracts/posts/Feed_Factory.sol
@@ -1,10 +1,48 @@
 pragma solidity ^0.5.0;
 
-import "../helpers/Spawner.sol";
+import "../modules/Factory.sol";
 import "./Feed.sol";
-import "../modules/iFactoryRegistry.sol";
 
 
-contract Feed_Factory is Spawner {
-    
+contract Feed_Factory is Factory {
+
+    constructor(address instanceRegistry) public {
+        // deploy template contract
+        address templateContract = address(new Feed());
+
+        // set instance type
+        bytes4 instanceType = bytes4(keccak256(bytes('Feed')));
+
+        // set initABI
+        string memory initABI = '(bytes4,address,address,bytes)';
+
+        // initialize factory params
+        Factory._initialize(instanceRegistry, templateContract, instanceType, initABI);
+    }
+
+    event ExplicitInitData(address operator, address postRegistry, bytes feedStaticMetadata);
+
+    function createExplicit(
+        address operator,
+        address postRegistry,
+        bytes memory feedStaticMetadata
+    ) public returns (address instance) {
+        // declare template in memory
+        Feed template;
+
+        // construct the data payload used when initializing the new contract.
+        bytes memory initData = abi.encodeWithSelector(
+            template.initialize.selector, // selector
+            operator,
+            postRegistry,
+            feedStaticMetadata
+        );
+
+        // deploy instance
+        instance = Factory.create(initData);
+
+        // emit event
+        emit ExplicitInitData(operator, postRegistry, feedStaticMetadata);
+    }
+
 }
