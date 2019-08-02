2c14b52b95c15e686d006b8d34411d1321fcfc81
James Geary <james@numer.ai>
2019-08-02 14:12:10 -0700

    Update deployment script (#106)
    * initial commit
    
    * tweak
    
    * fix Feed Registry name
    
    * update deployment yarn script
    
    * add deployment addresses

--- Fractal/contracts/registries/Erasure_Feeds.sol
+++ Fractal/contracts/registries/Erasure_Feeds.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "../modules/Registry.sol";
 
 
-contract Erasure_Posts is Registry {
+contract Erasure_Feeds is Registry {
 
     constructor() public Registry('Feed') {
 
