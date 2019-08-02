e9b00cd9ccf13733dc413f6d3ca5fab6eb5b8e8a
Stephane Gosselin <thegostep@gmail.com>
2019-08-02 13:43:26 -0700

    add token deployment script

---
+++ Fractal/deployment/deploy_MockNMR.js
@@ -0,0 +1,26 @@
+const etherlime = require('etherlime-lib');
+const ethers = require('ethers')
+require('dotenv').config()
+
+
+const deploy = async (network, secret) => {
+
+	let contracts = {
+		MockNMR: {artifact: require('../build/MockNMR.json')},
+	}
+
+	let deployer
+	let wallet = new ethers.Wallet(process.env.DEPLOYMENT_PRIV_KEY)
+	console.log(`Deployment Wallet: ${wallet.address}`)
+
+	if (network == 'rinkeby') {
+		deployer = await new etherlime.InfuraPrivateKeyDeployer(wallet.privateKey, 'rinkeby', process.env.INFURA_API_KEY)
+		deployer.setVerifierApiKey(process.env.ETHERSCAN_API_KEY)
+
+		contracts.MockNMR.instance = await deployer.deployAndVerify(contracts.MockNMR.artifact)
+	}
+}
+
+module.exports = {
+	deploy
+}
