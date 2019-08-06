2f6de6766ee7dce6e8b459ebf3e1c83724c98d8f
kenchangh <guanhao3797@gmail.com>
2019-08-07 00:32:32 +0800

    test for Factory.create

--- Fractal/test/modules/Factory.js
+++ Fractal/test/modules/Factory.js
@@ -1,11 +1,23 @@
 const { createDeployer } = require("../helpers/setup");
 const { RATIO_TYPES } = require("../helpers/variables");
+const {
+  createInstanceAddress,
+  createEip1167RuntimeCode
+} = require("../helpers/utils");
 
 const OneWayGriefing_FactoryArtifact = require("../../build/OneWayGriefing_Factory.json");
 const ErasureAgreementsRegistryArtifact = require("../../build/Erasure_Agreements.json");
 const ErasurePostsRegistryArtifact = require("../../build/Erasure_Posts.json");
-// const TestOneWayGriefingArtifact = require("../../build/TestOneWayGriefing.json");
-// const MockNMRArtifact = require("../../build/MockNMR.json");
+const MockNMRArtifact = require("../../build/MockNMR.json");
+
+const getLatestContractAdressFrom = async address => {
+  const nonce = await deployer.provider.getTransactionCount(address);
+  const contractAddress = ethers.utils.getContractAddress({
+    from: address,
+    nonce: nonce - 1
+  });
+  return contractAddress;
+};
 
 describe("Factory", function() {
   this.timeout(4000);
@@ -23,14 +35,27 @@ describe("Factory", function() {
   const ratioType = RATIO_TYPES.CgtP;
   const countdownLength = 1000;
   const staticMetadata = "TESTING";
+  const expectedABI =
+    "(bytes4,address,address,address,address,uint256,Griefing.RatioType,uint256,bytes)";
+
+  const createArgs = [
+    owner,
+    seller,
+    buyer,
+    ratio,
+    ratioType,
+    countdownLength,
+    Buffer.from(staticMetadata)
+  ];
 
   let deployer;
 
   before(async () => {
     deployer = createDeployer();
 
-    this.registry = await deployer.deploy(ErasureAgreementsRegistryArtifact);
-    this.wrongRegistry = await deployer.deploy(ErasurePostsRegistryArtifact);
+    this.MockNMR = await deployer.deploy(MockNMRArtifact);
+    this.Registry = await deployer.deploy(ErasureAgreementsRegistryArtifact);
+    this.WrongRegistry = await deployer.deploy(ErasurePostsRegistryArtifact);
   });
 
   describe("Factory._initialize", () => {
@@ -39,7 +64,7 @@ describe("Factory", function() {
         deployer.deploy(
           OneWayGriefing_FactoryArtifact,
           false,
-          this.wrongRegistry.contractAddress
+          this.WrongRegistry.contractAddress
         ),
         "incorrect instance type"
       );
@@ -49,7 +74,7 @@ describe("Factory", function() {
       this.OWGFactory = await deployer.deploy(
         OneWayGriefing_FactoryArtifact,
         false,
-        this.registry.contractAddress
+        this.Registry.contractAddress
       );
 
       // Factory.getInstanceType
@@ -68,26 +93,73 @@ describe("Factory", function() {
       );
 
       // Factory.getInitABI
-      const expectedABI =
-        "(bytes4,address,address,address,address,uint256,Griefing.RatioType,uint256,bytes)";
       const actualABI = await this.OWGFactory.getInitABI();
       assert.equal(actualABI, expectedABI);
 
       // Factory.getInstanceRegistry
       const actualInstanceRegistry = await this.OWGFactory.getInstanceRegistry();
-      assert.equal(actualInstanceRegistry, this.registry.contractAddress);
+      assert.equal(actualInstanceRegistry, this.Registry.contractAddress);
 
       // Factory.getTemplate
-      const nonce = await deployer.provider.getTransactionCount(
+      const expectedTemplateAddress = await getLatestContractAdressFrom(
         this.OWGFactory.contractAddress
       );
-      const expectedTemplateAddress = ethers.utils.getContractAddress({
-        from: this.OWGFactory.contractAddress,
-        nonce: nonce - 1
-      });
-
       const actualTemplateAddress = await this.OWGFactory.getTemplate();
       assert.equal(actualTemplateAddress, expectedTemplateAddress);
+    });
+  });
+
+  describe("Factory.create", () => {
+    it("should create instance correctly", async () => {
+      await this.Registry.addFactory(
+        this.OWGFactory.contractAddress,
+        Buffer.from("")
+      );
+
+      const logicContract = await this.OWGFactory.getTemplate();
+
+      // seller creates the OneWayGriefing instance
+      const txn = await this.OWGFactory.from(seller).createExplicit(
+        this.MockNMR.contractAddress,
+        ...createArgs
+      );
+
+      const receipt = await this.OWGFactory.verboseWaitForTransaction(txn);
+
+      const expectedEvent = "InstanceCreated";
+      const instanceCreatedEvent = receipt.events.find(
+        emittedEvent => emittedEvent.event === expectedEvent,
+        "There is no such event"
+      );
+
+      assert.isDefined(instanceCreatedEvent);
+      assert.equal(instanceCreatedEvent.args.creator, seller);
+      assert.equal(instanceCreatedEvent.args.initABI, expectedABI);
+
+      const { targetAddress, initData } = createInstanceAddress(
+        seller,
+        this.OWGFactory.contractAddress,
+        logicContract,
+        "initialize",
+        [
+          "address",
+          "address",
+          "address",
+          "address",
+          "uint256",
+          "uint8",
+          "uint256",
+          "bytes"
+        ],
+        [this.MockNMR.contractAddress, ...createArgs],
+        0
+      );
+      assert.equal(instanceCreatedEvent.args.instance, targetAddress);
+      assert.equal(instanceCreatedEvent.args.initData, initData);
+
+      const actualRuntimeCode = await deployer.provider.getCode(targetAddress);
+      const runtimeCode = createEip1167RuntimeCode(logicContract);
+      assert.equal(actualRuntimeCode, runtimeCode);
     });
   });
 });
