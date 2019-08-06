7e1f1222ce2cec845d4f924b281538284aa198c7
Stephane Gosselin <thegostep@gmail.com>
2019-08-06 12:52:55 -0700

    ProofHash module fixes #121

---
+++ Fractal/contracts/modules/ProofHash.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+import "./MultiHashWrapper.sol";
+
+
+contract ProofHash is MultiHashWrapper {
+
+    MultiHash private _proofHash;
+
+    event ProofHashSet(address caller, bytes proofHash);
+
+    // state functions
+
+    function _setProofHash(bytes memory proofHash) internal {
+        _proofHash = MultiHashWrapper._splitMultiHash(proofHash);
+        emit ProofHashSet(msg.sender, proofHash);
+    }
+
+    // view functions
+
+    function getProofHash() public view returns (bytes memory proofHash) {
+        proofHash = MultiHashWrapper._combineMultiHash(_proofHash);
+    }
+
+}
