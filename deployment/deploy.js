92396d6451a7ac161d163b23c8b1bf7f510c73fb
Stephane Gosselin <thegostep@gmail.com>
2019-08-24 01:14:50 +0200

    add mainnet deployment scripts

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -3,10 +3,9 @@ const ethers = require("ethers");
 const { createMultihashSha256 } = require("../test/helpers/utils");
 require("dotenv").config();
 
-const tokenAddress = "0x1eBf22785bffb6B44fEbBc8a41056b1aD43401f9";
-
 const deploy = async (network, secret) => {
   let contracts = {
+    MockNMR: {artifact: require('../build/MockNMR.json')},
     Erasure_Agreements: { artifact: require("../build/Erasure_Agreements.json") },
     Erasure_Escrows: { artifact: require("../build/Erasure_Escrows.json") },
     Erasure_Posts: { artifact: require("../build/Erasure_Posts.json") },
@@ -19,11 +18,16 @@ const deploy = async (network, secret) => {
     Post: { artifact: require("../build/Post.json") }
   };
 
+  let tokenAddress;
   let deployer;
   let wallet = new ethers.Wallet(process.env.DEPLOYMENT_PRIV_KEY);
+  let multisig;
   console.log(`Deployment Wallet: ${wallet.address}`);
 
   if (network == "rinkeby") {
+
+    tokenAddress = "0x1eBf22785bffb6B44fEbBc8a41056b1aD43401f9";
+    multisig = "0x6087555A70E2F96B7838806e7743041E035a37e5";
 
     // connect to rinkeby
     wallet = await wallet.connect(ethers.getDefaultProvider('rinkeby'));
@@ -39,9 +43,37 @@ const deploy = async (network, secret) => {
     // contracts.Erasure_Posts.instance = await new ethers.Contract('0xEd11206a7d07601985DCC4FeD7B3284a928D022B', contracts.Erasure_Posts.artifact.abi, wallet);
 
     // deploy registries
-    contracts.Erasure_Posts.instance = await deployer.deployAndVerify(contracts.Erasure_Posts.artifact, false);
-    contracts.Erasure_Agreements.instance = await deployer.deployAndVerify(contracts.Erasure_Agreements.artifact, false);
-    contracts.Erasure_Users.instance = await deployer.deployAndVerify(contracts.Erasure_Users.artifact, false);
+    contracts.Erasure_Posts.instance = await deployer.deployAndVerify(contracts.Erasure_Posts.artifact);
+    contracts.Erasure_Agreements.instance = await deployer.deployAndVerify(contracts.Erasure_Agreements.artifact);
+    contracts.Erasure_Users.instance = await deployer.deployAndVerify(contracts.Erasure_Users.artifact);
+
+    // deploy factories
+    contracts.Post_Factory.instance = await deployer.deployAndVerify(contracts.Post_Factory.artifact, false, contracts.Erasure_Posts.instance.contractAddress);
+    contracts.Feed_Factory.instance = await deployer.deployAndVerify(contracts.Feed_Factory.artifact, false, contracts.Erasure_Posts.instance.contractAddress);
+    contracts.OneWayGriefing_Factory.instance = await deployer.deployAndVerify(contracts.OneWayGriefing_Factory.artifact, false, contracts.Erasure_Agreements.instance.contractAddress);
+
+} else if (network == "mainnet") {
+
+    tokenAddress = "0x1776e1F26f98b1A5dF9cD347953a26dd3Cb46671";
+    multisig = "0x0000000000377d181a0ebd08590c6b399b272000";
+
+    // connect to rinkeby
+    wallet = await wallet.connect(ethers.getDefaultProvider('mainnet'));
+
+    // initialize deployer
+    deployer = await new etherlime.InfuraPrivateKeyDeployer(
+      wallet.privateKey,
+      "mainnet",
+      process.env.INFURA_API_KEY
+    );
+    deployer.setVerifierApiKey(process.env.ETHERSCAN_API_KEY);
+
+    // contracts.Erasure_Posts.instance = await new ethers.Contract('0xEd11206a7d07601985DCC4FeD7B3284a928D022B', contracts.Erasure_Posts.artifact.abi, wallet);
+
+    // deploy registries
+    contracts.Erasure_Posts.instance = await deployer.deployAndVerify(contracts.Erasure_Posts.artifact);
+    contracts.Erasure_Agreements.instance = await deployer.deployAndVerify(contracts.Erasure_Agreements.artifact);
+    contracts.Erasure_Users.instance = await deployer.deployAndVerify(contracts.Erasure_Users.artifact);
 
     // deploy factories
     contracts.Post_Factory.instance = await deployer.deployAndVerify(contracts.Post_Factory.artifact, false, contracts.Erasure_Posts.instance.contractAddress);
@@ -49,13 +81,20 @@ const deploy = async (network, secret) => {
     contracts.OneWayGriefing_Factory.instance = await deployer.deployAndVerify(contracts.OneWayGriefing_Factory.artifact, false, contracts.Erasure_Agreements.instance.contractAddress);
 
 } else if (network == "ganache") {
+
+    multisig = wallet.address;
+
     // initialize deployer
     deployer = new etherlime.EtherlimeGanacheDeployer();
 
+    // deploy mock NMR
+    contracts.MockNMR.instance = await deployer.deploy(contracts.MockNMR.artifact);
+    tokenAddress = contracts.MockNMR.instance.contractAddress;
+
     // deploy registries
-    contracts.Erasure_Posts.instance = await deployer.deploy(contracts.Erasure_Posts.artifact, false);
-    contracts.Erasure_Agreements.instance = await deployer.deploy(contracts.Erasure_Agreements.artifact, false);
-    contracts.Erasure_Users.instance = await deployer.deploy(contracts.Erasure_Users.artifact, false);
+    contracts.Erasure_Posts.instance = await deployer.deploy(contracts.Erasure_Posts.artifact);
+    contracts.Erasure_Agreements.instance = await deployer.deploy(contracts.Erasure_Agreements.artifact);
+    contracts.Erasure_Users.instance = await deployer.deploy(contracts.Erasure_Users.artifact);
 
     // deploy factories
     contracts.Post_Factory.instance = await deployer.deploy(contracts.Post_Factory.artifact, false, contracts.Erasure_Posts.instance.contractAddress);
@@ -63,29 +102,54 @@ const deploy = async (network, secret) => {
     contracts.OneWayGriefing_Factory.instance = await deployer.deploy(contracts.OneWayGriefing_Factory.artifact, false, contracts.Erasure_Agreements.instance.contractAddress);
   }
 
-  // register factories
+  console.log(`
+Register Factories
+      `);
 
   await contracts.Erasure_Posts.instance.addFactory(
     contracts.Post_Factory.instance.contractAddress,
     ethers.utils.hexlify(0x0)
-  );
-  console.log('Post_Factory is registered');
+  ).then(async txn => {
+      console.log(`addFactory() | Post_Factory => Erasure_Posts`);
+      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+  });
+
   await contracts.Erasure_Posts.instance.addFactory(
     contracts.Feed_Factory.instance.contractAddress,
     ethers.utils.hexlify(0x0)
-  );
-  console.log('Feed_Factory is registered');
+  ).then(async txn => {
+      console.log(`addFactory() | Feed_Factory => Erasure_Posts`);
+      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+  });
+
   await contracts.Erasure_Agreements.instance.addFactory(
     contracts.OneWayGriefing_Factory.instance.contractAddress,
     ethers.utils.hexlify(0x0)
-  );
-  console.log('OneWayGriefing_Factory is registered');
+  ).then(async txn => {
+      console.log(`addFactory() | OneWayGriefing_Factory => Erasure_Agreements`);
+      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+  });
 
-  // attempt to create instances
+  console.log(`
+Transfer Registry Ownership
+      `);
+
+  await contracts.Erasure_Posts.instance.transferOwnership(multisig).then(async txn => {
+      console.log(`transferOwnership() | Erasure_Posts => ${multisig}`);
+      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+  });
+  await contracts.Erasure_Agreements.instance.transferOwnership(multisig).then(async txn => {
+      console.log(`transferOwnership() | Erasure_Agreements => ${multisig}`);
+      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+  });
+
+  console.log(`
+Create test instances from factories
+      `);
 
   const userAddress = '0x6087555A70E2F96B7838806e7743041E035a37e5';
   const proofHash = createMultihashSha256("proofHash");
-  console.log(proofHash);
+  console.log(`multihash: ${proofHash}`);
 
   await contracts.Post_Factory.instance.createExplicit(
       userAddress,
@@ -100,7 +164,7 @@ const deploy = async (network, secret) => {
         "There is no such event"
       );
       contracts.Post.instance = new ethers.Contract(eventFound.args.instance, contracts.Post.artifact.abi, deployer.provider);
-      console.log(`Post instance created at ${eventFound.args.instance} by ${await contracts.Post.instance.getCreator()}`);
+      console.log(`createExplicit() | Post_Factory => ${eventFound.args.instance}`);
   });
 
   await contracts.Feed_Factory.instance.createExplicit(
@@ -115,7 +179,7 @@ const deploy = async (network, secret) => {
         "There is no such event"
       );
       contracts.Feed.instance = new ethers.Contract(eventFound.args.instance, contracts.Feed.artifact.abi, deployer.provider);
-      console.log(`Feed instance created at ${eventFound.args.instance} by ${await contracts.Feed.instance.getCreator()}`);
+      console.log(`createExplicit() | Feed_Factory => ${eventFound.args.instance}`);
   });
 
   await contracts.OneWayGriefing_Factory.instance.createExplicit(
@@ -135,11 +199,11 @@ const deploy = async (network, secret) => {
         "There is no such event"
       );
       contracts.OneWayGriefing.instance = new ethers.Contract(eventFound.args.instance, contracts.OneWayGriefing.artifact.abi, deployer.provider);
-      console.log(`OneWayGriefing instance created at ${eventFound.args.instance} by ${await contracts.OneWayGriefing.instance.getCreator()}`);
+      console.log(`createExplicit() | OneWayGriefing_Factory => ${eventFound.args.instance}`);
   });
 
+  console.log(``);
+
 };
 
-module.exports = {
-  deploy
-};
+module.exports = { deploy };
