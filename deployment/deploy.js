88798ba9c2de31d9b72adae165446bd947d501b0
Stephane Gosselin <thegostep@gmail.com>
2019-08-24 09:29:40 +0200

    fix deployment scripts

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -24,6 +24,8 @@ const deploy = async (network, secret) => {
   let multisig;
   console.log(`Deployment Wallet: ${wallet.address}`);
 
+  let defaultGas = ethers.utils.parseUnits('10', 'gwei');
+
   if (network == "rinkeby") {
 
     tokenAddress = "0x1eBf22785bffb6B44fEbBc8a41056b1aD43401f9";
@@ -58,17 +60,23 @@ const deploy = async (network, secret) => {
     multisig = "0x0000000000377d181a0ebd08590c6b399b272000";
 
     // connect to rinkeby
-    wallet = await wallet.connect(ethers.getDefaultProvider('mainnet'));
+    wallet = await wallet.connect(ethers.getDefaultProvider('homestead'));
 
     // initialize deployer
     deployer = await new etherlime.InfuraPrivateKeyDeployer(
       wallet.privateKey,
       "mainnet",
-      process.env.INFURA_API_KEY
+      process.env.INFURA_API_KEY,
+      { gasPrice: defaultGas }
     );
     deployer.setVerifierApiKey(process.env.ETHERSCAN_API_KEY);
 
-    // contracts.Erasure_Posts.instance = await new ethers.Contract('0xEd11206a7d07601985DCC4FeD7B3284a928D022B', contracts.Erasure_Posts.artifact.abi, wallet);
+    // contracts.Erasure_Posts.instance = await new ethers.Contract('0xF9F79CEe69175B51E4aB2b2D231460B61Ebb2688', contracts.Erasure_Posts.artifact.abi, wallet);
+    // contracts.Erasure_Agreements.instance = await new ethers.Contract('0xF9F79CEe69175B51E4aB2b2D231460B61Ebb2688', contracts.Erasure_Agreements.artifact.abi, wallet);
+    // contracts.Erasure_Users.instance = await new ethers.Contract('0xD5fa2751A560b6ca47219224Bc231c56a9849492', contracts.Erasure_Users.artifact.abi, wallet);
+    // contracts.Post_Factory.instance = await new ethers.Contract('0x750EC6138205Cf1C36b446Dd540E0464E9C0e6Cd', contracts.Post_Factory.artifact.abi, wallet);
+    // contracts.Feed_Factory.instance = await new ethers.Contract('0x276e1fdB65951B8c1d1c16C5B69a72bE3060E7AA', contracts.Feed_Factory.artifact.abi, wallet);
+    // contracts.OneWayGriefing_Factory.instance = await new ethers.Contract('0x3A1a3EfeDf5C3932Bac1b637EA8Ac2D904C58480', contracts.OneWayGriefing_Factory.artifact.abi, wallet);
 
     // deploy registries
     contracts.Erasure_Posts.instance = await deployer.deployAndVerify(contracts.Erasure_Posts.artifact);
@@ -111,36 +119,44 @@ Register Factories
     ethers.utils.hexlify(0x0)
   ).then(async txn => {
       console.log(`addFactory() | Post_Factory => Erasure_Posts`);
-      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+      const receipt = await contracts.Erasure_Posts.instance.verboseWaitForTransaction(txn);
   });
 
   await contracts.Erasure_Posts.instance.addFactory(
     contracts.Feed_Factory.instance.contractAddress,
-    ethers.utils.hexlify(0x0)
+    ethers.utils.hexlify(0x0),
+    { gasPrice: defaultGas }
   ).then(async txn => {
       console.log(`addFactory() | Feed_Factory => Erasure_Posts`);
-      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+      const receipt = await contracts.Erasure_Posts.instance.verboseWaitForTransaction(txn);
   });
 
   await contracts.Erasure_Agreements.instance.addFactory(
     contracts.OneWayGriefing_Factory.instance.contractAddress,
-    ethers.utils.hexlify(0x0)
+    ethers.utils.hexlify(0x0),
+    { gasPrice: defaultGas }
   ).then(async txn => {
       console.log(`addFactory() | OneWayGriefing_Factory => Erasure_Agreements`);
-      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+      const receipt = await contracts.Erasure_Agreements.instance.verboseWaitForTransaction(txn);
   });
 
   console.log(`
 Transfer Registry Ownership
       `);
 
-  await contracts.Erasure_Posts.instance.transferOwnership(multisig).then(async txn => {
+  await contracts.Erasure_Posts.instance.transferOwnership(
+      multisig,
+      { gasPrice: defaultGas }
+  ).then(async txn => {
       console.log(`transferOwnership() | Erasure_Posts => ${multisig}`);
-      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+      const receipt = await contracts.Erasure_Posts.instance.verboseWaitForTransaction(txn);
   });
-  await contracts.Erasure_Agreements.instance.transferOwnership(multisig).then(async txn => {
+  await contracts.Erasure_Agreements.instance.transferOwnership(
+      multisig,
+      { gasPrice: defaultGas }
+  ).then(async txn => {
       console.log(`transferOwnership() | Erasure_Agreements => ${multisig}`);
-      const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
+      const receipt = await contracts.Erasure_Agreements.instance.verboseWaitForTransaction(txn);
   });
 
   console.log(`
@@ -155,7 +171,8 @@ Create test instances from factories
       userAddress,
       proofHash,
       '0x0',
-      '0x0'
+      '0x0',
+      { gasPrice: defaultGas }
   ).then(async txn => {
       const receipt = await contracts.Post_Factory.instance.verboseWaitForTransaction(txn);
       const expectedEvent = "InstanceCreated";
@@ -170,7 +187,8 @@ Create test instances from factories
   await contracts.Feed_Factory.instance.createExplicit(
       userAddress,
       contracts.Erasure_Posts.instance.contractAddress,
-      '0x0'
+      '0x0',
+      { gasPrice: defaultGas }
   ).then(async txn => {
       const receipt = await contracts.Feed_Factory.instance.verboseWaitForTransaction(txn);
       const expectedEvent = "InstanceCreated";
@@ -190,7 +208,8 @@ Create test instances from factories
       1,
       2,
       100000000,
-      '0x0'
+      '0x0',
+      { gasPrice: defaultGas }
   ).then(async txn => {
       const receipt = await contracts.OneWayGriefing_Factory.instance.verboseWaitForTransaction(txn);
       const expectedEvent = "InstanceCreated";
