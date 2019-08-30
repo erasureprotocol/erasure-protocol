f95d74e599124b3546689db3c4c8ca6544799306
Stephane Gosselin <thegostep@gmail.com>
2019-08-30 12:38:24 -0700

    fix and improve

--- contracts/wrappers/FeedCreator.sol
+++ contracts/wrappers/FeedCreator.sol
@@ -6,6 +6,13 @@ import "../helpers/openzeppelin-solidity/ownership/Ownable.sol";
 contract FeedManager is Ownable {
 
     address private _feed;
+    address private _feedFactory;
+    address private _postFactory;
+
+    constructor(address feedFactory, address postFactory) public {
+        _feedFactory = feedFactory;
+        _postFactory = postFactory;
+    }
 
     function createFeed(
         address operator,
@@ -24,17 +31,36 @@ contract FeedManager is Ownable {
         );
 
         // deploy instance
-        instance = Factory.create(callData);
+        instance = Feed_Factory(_feedFactory).create(callData);
 
         // set instance in storage
-        _feed = instance
+        _feed = instance;
     }
 
     function createPost(address postFactory, bytes memory initData) public returns (address post) {
-        post = Feed(feed).createPost(postFactory, initData);
+        post = Feed(_feed).createPost(postFactory, initData);
     }
 
-    function getFeed() public returns (address feed) {
+    function createPostExplicit(
+        address operator,
+        bytes memory proofHash,
+        bytes memory staticMetadata,
+        bytes memory variableMetadata
+    ) public returns (address post) {
+
+        // construct the data payload used when initializing the new contract.
+        bytes memory initData = abi.encode(
+            operator,
+            proofHash,
+            staticMetadata,
+            variableMetadata
+        );
+
+        // deploy instance
+        post = Feed(_feed).createPost(_postFactory, initData);
+    }
+
+    function getFeed() public view returns (address feed) {
         feed = _feed;
     }
 }
