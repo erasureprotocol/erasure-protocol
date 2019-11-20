a99ff0f7aeb2f0ca7f9385dc19de966082950d43
Stephane Gosselin <thegostep@gmail.com>
2019-11-20 14:02:13 -0800

    remove TODO fixes #291

--- contracts/modules/iFactory.sol
+++ contracts/modules/iFactory.sol
@@ -1,29 +1,25 @@
 pragma solidity ^0.5.0;
 
-/* TODO: Update eip165 interface
- * bytes4(keccak256('create(bytes)')) == 0xcf5ba53f
- * bytes4(keccak256('getInstanceType()')) == 0x18c2f4cf
- * bytes4(keccak256('getInstanceRegistry()')) == 0xa5e13904
- * bytes4(keccak256('getImplementation()')) == 0xaaf10f42
- *
- * => 0xcf5ba53f ^ 0x18c2f4cf ^ 0xa5e13904 ^ 0xaaf10f42 == 0xd88967b6
- */
- interface iFactory {
+/// @title iFactory
+/// @author Stephane Gosselin (@thegostep) for Numerai Inc
+/// @dev Security contact: security@numer.ai
+/// @dev Version: 1.2.0
+interface iFactory {
 
-     event InstanceCreated(address indexed instance, address indexed creator, string initABI, bytes initData);
+    event InstanceCreated(address indexed instance, address indexed creator, string initABI, bytes initData);
 
-     function create(bytes calldata initData) external returns (address instance);
-     function createSalty(bytes calldata initData, bytes32 salt) external returns (address instance);
-     function getInitSelector() external view returns (bytes4 initSelector);
-     function getInstanceRegistry() external view returns (address instanceRegistry);
-     function getTemplate() external view returns (address template);
-     function getSaltyInstance(bytes calldata, bytes32 salt) external view returns (address instance);
-     function getNextInstance(bytes calldata) external view returns (address instance);
+    function create(bytes calldata initData) external returns (address instance);
+    function createSalty(bytes calldata initData, bytes32 salt) external returns (address instance);
+    function getInitSelector() external view returns (bytes4 initSelector);
+    function getInstanceRegistry() external view returns (address instanceRegistry);
+    function getTemplate() external view returns (address template);
+    function getSaltyInstance(bytes calldata, bytes32 salt) external view returns (address instance);
+    function getNextInstance(bytes calldata) external view returns (address instance);
 
-     function getInstanceCreator(address instance) external view returns (address creator);
-     function getInstanceType() external view returns (bytes4 instanceType);
-     function getInstanceCount() external view returns (uint256 count);
-     function getInstance(uint256 index) external view returns (address instance);
-     function getInstances() external view returns (address[] memory instances);
-     function getPaginatedInstances(uint256 startIndex, uint256 endIndex) external view returns (address[] memory instances);
- }
+    function getInstanceCreator(address instance) external view returns (address creator);
+    function getInstanceType() external view returns (bytes4 instanceType);
+    function getInstanceCount() external view returns (uint256 count);
+    function getInstance(uint256 index) external view returns (address instance);
+    function getInstances() external view returns (address[] memory instances);
+    function getPaginatedInstances(uint256 startIndex, uint256 endIndex) external view returns (address[] memory instances);
+}
--- contracts/modules/iRegistry.sol
+++ contracts/modules/iRegistry.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.5.0;
 
 
+/// @title iRegistry
+/// @author Stephane Gosselin (@thegostep) for Numerai Inc
+/// @dev Security contact: security@numer.ai
+/// @dev Version: 1.2.0
 interface iRegistry {
 
     enum FactoryStatus { Unregistered, Registered, Retired }
