22f3d11b1f20f7642b3e814f37995412fa51f35b
Stephane Gosselin <thegostep@gmail.com>
2019-11-20 17:04:35 -0800

    clarify burn behavior fixes #293

--- contracts/modules/iNMR.sol
+++ contracts/modules/iNMR.sol
@@ -17,8 +17,23 @@ contract iNMR {
     function approve(address _spender, uint256 _value) external returns (bool ok);
     function changeApproval(address _spender, uint256 _oldValue, uint256 _newValue) external returns (bool ok);
 
-    // burn
+    /// @dev Behavior has changed to match OpenZeppelin's `ERC20Burnable.burn(uint256 amount)`
+    /// @dev Destoys `amount` tokens from `msg.sender`, reducing the total supply.
+    ///
+    /// Emits a `Transfer` event with `to` set to the zero address.
+    /// Requirements:
+    /// - `account` must have at least `amount` tokens.
     function mint(uint256 _value) external returns (bool ok);
-    // burnFrom
+
+    /// @dev Behavior has changed to match OpenZeppelin's `ERC20Burnable.burnFrom(address account, uint256 amount)`
+    /// @dev Destoys `amount` tokens from `account`.`amount` is then deducted
+    /// from the caller's allowance.
+    ///
+    /// Emits an `Approval` event indicating the updated allowance.
+    /// Emits a `Transfer` event with `to` set to the zero address.
+    ///
+    /// Requirements:
+    /// - `account` must have at least `amount` tokens.
+    /// - `account` must have approved `msg.sender` with allowance of at least `amount` tokens.
     function numeraiTransfer(address _to, uint256 _value) external returns (bool ok);
 }
