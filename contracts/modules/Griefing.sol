0f963d879dcb5c650c2e898ff8675cc90afcdb31
Stephane Gosselin <thegostep@gmail.com>
2019-11-21 13:57:18 -0800

    clarify getPunishment fix #276

--- contracts/modules/Griefing.sol
+++ contracts/modules/Griefing.sol
@@ -26,6 +26,14 @@ contract Griefing is Staking {
 
     // state functions
 
+    /// @notice Set the grief ratio and type for a given staker
+    /// @param staker Address of the staker
+    /// @param ratio Uint256 number (18 decimals)
+    ///              NOTE: ratio must be 0 if ratioType is Inf or NaN
+    /// @param ratioType Griefing.RatioType number. Ratio Type must be one of the following three values:
+    ///                   - Dec: Ratio is a decimal number with 18 decimals
+    ///                   - Inf: Punishment at no cost
+    ///                   - NaN: No Punishment
     function _setRatio(address staker, uint256 ratio, RatioType ratioType) internal {
         if (ratioType == RatioType.NaN || ratioType == RatioType.Inf) {
             require(ratio == 0, "ratio must be 0 when ratioType is NaN or Inf");
@@ -39,6 +47,13 @@ contract Griefing is Staking {
         emit RatioSet(staker, ratio, ratioType);
     }
 
+    /// @notice Punish a stake through griefing
+    ///         NOTE: the cost of the punishment is taken form the account of the punisher. This therefore requires appropriate ERC-20 token approval.
+    /// @param punisher Address of the punisher
+    /// @param staker Address of the staker
+    /// @param punishment Amount of NMR (18 decimals) to punish
+    /// @param message Bytes reason string for the punishment
+    /// @return cost Amount of NMR (18 decimals) to pay
     function _grief(
         address punisher,
         address staker,
@@ -70,6 +85,13 @@ contract Griefing is Staking {
 
     // view functions
 
+    /// @notice Get the ratio of a staker
+    /// @param staker Address of the staker
+    /// @return ratio Uint256 number (18 decimals)
+    /// @return ratioType Griefing.RatioType number. Ratio Type must be one of the following three values:
+    ///                   - Dec: Ratio is a decimal number with 18 decimals
+    ///                   - Inf: Punishment at no cost
+    ///                   - NaN: No Punishment
     function getRatio(address staker) public view returns (uint256 ratio, RatioType ratioType) {
         // get stake data from storage
         return (_griefRatio[staker].ratio, _griefRatio[staker].ratioType);
@@ -77,10 +99,15 @@ contract Griefing is Staking {
 
     // pure functions
 
+    /// @notice Get exact cost for a given punishment and ratio
+    /// @param ratio Uint256 number (18 decimals)
+    /// @param punishment Amount of NMR (18 decimals) to punish
+    /// @param ratioType Griefing.RatioType number. Ratio Type must be one of the following three values:
+    ///                   - Dec: Ratio is a decimal number with 18 decimals
+    ///                   - Inf: Punishment at no cost
+    ///                   - NaN: No Punishment
+    /// @return cost Amount of NMR (18 decimals) to pay
     function getCost(uint256 ratio, uint256 punishment, RatioType ratioType) public pure returns(uint256 cost) {
-        /* Dec: Cost multiplied by ratio interpreted as a decimal number with 18 decimals, e.g. 1 -> 1e18
-         * Inf: Punishment at no cost
-         * NaN: No Punishment */
         if (ratioType == RatioType.Dec) {
             return DecimalMath.mul(SafeMath.mul(punishment, e18), ratio) / e18;
         }
@@ -90,10 +117,16 @@ contract Griefing is Staking {
             revert("ratioType cannot be RatioType.NaN");
     }
 
+    /// @notice Get approximate punishment for a given cost and ratio.
+    ///         The punishment is an approximate value due to quantization / rounding.
+    /// @param ratio Uint256 number (18 decimals)
+    /// @param cost Amount of NMR (18 decimals) to pay
+    /// @param ratioType Griefing.RatioType number. Ratio Type must be one of the following three values:
+    ///                   - Dec: Ratio is a decimal number with 18 decimals
+    ///                   - Inf: Punishment at no cost
+    ///                   - NaN: No Punishment
+    /// @return punishment Approximate amount of NMR (18 decimals) to punish
     function getPunishment(uint256 ratio, uint256 cost, RatioType ratioType) public pure returns(uint256 punishment) {
-        /* Dec: Ratio is a decimal number with 18 decimals
-         * Inf: Punishment at no cost
-         * NaN: No Punishment */
         if (ratioType == RatioType.Dec) {
             return DecimalMath.div(SafeMath.mul(cost, e18), ratio) / e18;
         }
