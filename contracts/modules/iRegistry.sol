41a83dd3cf136698920e416bf25ed8f46752231c
Stephane Gosselin <thegostep@gmail.com>
2020-01-08 19:56:13 +0200

    bump protocol version

--- contracts/modules/iRegistry.sol
+++ contracts/modules/iRegistry.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.5.13;
 /// @title iRegistry
 /// @author Stephane Gosselin (@thegostep) for Numerai Inc
 /// @dev Security contact: security@numer.ai
-/// @dev Version: 1.2.0
+/// @dev Version: 1.3.0
 interface iRegistry {
 
     enum FactoryStatus { Unregistered, Registered, Retired }
