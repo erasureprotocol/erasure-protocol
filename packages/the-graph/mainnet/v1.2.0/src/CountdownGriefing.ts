7815d75c312f4807cd63f6e7f78d19586006beba
propulsor <propulsor.ichimoku@gmail.com>
2020-01-11 11:43:29 -0700

    Adding mainnet v1.2.0 and update addresses for rinkeby v1.2.0

---
+++ packages/the-graph/mainnet/v1.2.0/src/CountdownGriefing.ts
@@ -0,0 +1,240 @@
+import { BigInt } from '@graphprotocol/graph-ts'
+import { InstanceCreated } from '../generated/CountdownGriefingFactory/CountdownGriefingFactory'
+import {
+  Initialized,
+  RatioSet,
+  Griefed,
+  LengthSet,
+  OperatorUpdated,
+  MetadataSet,
+  StakeAdded,
+  StakeTaken,
+  StakeBurned,
+  DeadlineSet,
+} from '../generated/templates/CountdownGriefing/CountdownGriefing'
+import { CountdownGriefing as CountdownGriefingDataSource } from '../generated/templates'
+import {
+  CountdownGriefing,
+  InstanceCreatedCountdownGriefingFactory,
+  InitializedCountdownGriefing,
+  RatioSetCountdownGriefing,
+  GriefedCountdownGriefing,
+  LengthSetCountdownGriefing,
+  OperatorUpdatedCountdownGriefing,
+  MetadataSetCountdownGriefing,
+  StakeAddedCountdownGriefing,
+  StakeTakenCountdownGriefing,
+  StakeBurnedCountdownGriefing,
+  DeadlineSetCountdownGriefing,
+} from '../generated/schema'
+
+export function handleInstanceCreated(event: InstanceCreated): void {
+  let entity = new InstanceCreatedCountdownGriefingFactory(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.instance = event.params.instance
+  entity.creator = event.params.creator
+  entity.callData = event.params.callData
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let countdownGriefing = new CountdownGriefing(entity.instance.toHex())
+  countdownGriefing.creator = entity.creator
+  countdownGriefing.initCallData = entity.callData
+  countdownGriefing.createdTimestamp = entity.timestamp
+  countdownGriefing.save()
+
+  CountdownGriefingDataSource.create(event.params.instance)
+}
+
+export function handleInitialized(event: Initialized): void {
+  let entity = new InitializedCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.operator = event.params.operator
+  entity.staker = event.params.staker
+  entity.counterparty = event.params.counterparty
+  entity.ratio = event.params.ratio
+  entity.ratioType = event.params.ratioType
+  entity.countdownLength = event.params.countdownLength
+  entity.metadata = event.params.metadata
+  entity.metadataB58 = event.params.metadata.toBase58()
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let countdownGriefing = new CountdownGriefing(event.address.toHex())
+  countdownGriefing.staker = entity.staker
+  countdownGriefing.counterparty = entity.counterparty
+  countdownGriefing.ratio = entity.ratio
+  countdownGriefing.ratioType = entity.ratioType
+  countdownGriefing.countdownLength = entity.countdownLength
+  countdownGriefing.initMetadata = entity.metadata
+  countdownGriefing.initMetadataB58 = entity.metadataB58
+  countdownGriefing.currentStake = BigInt.fromI32(0)
+  countdownGriefing.totalBurned = BigInt.fromI32(0)
+  countdownGriefing.totalTaken = BigInt.fromI32(0)
+  countdownGriefing.save()
+}
+
+export function handleRatioSet(event: RatioSet): void {
+  let entity = new RatioSetCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.staker = event.params.staker
+  entity.ratio = event.params.ratio
+  entity.ratioType = event.params.ratioType
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+}
+
+export function handleGriefed(event: Griefed): void {
+  let entity = new GriefedCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.punisher = event.params.punisher
+  entity.staker = event.params.staker
+  entity.punishment = event.params.punishment
+  entity.cost = event.params.cost
+  entity.message = event.params.message
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+}
+
+export function handleLengthSet(event: LengthSet): void {
+  let entity = new LengthSetCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.length = event.params.length
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+}
+
+export function handleOperatorUpdated(event: OperatorUpdated): void {
+  let entity = new OperatorUpdatedCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.operator = event.params.operator
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let countdownGriefing = new CountdownGriefing(event.address.toHex())
+  countdownGriefing.operator = entity.operator
+  countdownGriefing.save()
+}
+
+export function handleMetadataSet(event: MetadataSet): void {
+  let entity = new MetadataSetCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.metadata = event.params.metadata
+  entity.metadataB58 = event.params.metadata.toBase58()
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let countdownGriefing = new CountdownGriefing(event.address.toHex())
+  countdownGriefing.metadata = entity.metadata
+  countdownGriefing.initMetadataB58 = entity.metadataB58
+  countdownGriefing.save()
+}
+
+export function handleStakeAdded(event: StakeAdded): void {
+  let entity = new StakeAddedCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.staker = event.params.staker
+  entity.funder = event.params.funder
+  entity.amount = event.params.amount
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let countdownGriefing = new CountdownGriefing(event.address.toHex())
+  countdownGriefing.currentStake = countdownGriefing.currentStake.plus(
+    entity.amount,
+  )
+  countdownGriefing.save()
+}
+
+export function handleStakeTaken(event: StakeTaken): void {
+  let entity = new StakeTakenCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.staker = event.params.staker
+  entity.recipient = event.params.recipient
+  entity.amount = event.params.amount
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let countdownGriefing = new CountdownGriefing(event.address.toHex())
+  countdownGriefing.currentStake = countdownGriefing.currentStake.minus(
+    entity.amount,
+  )
+  countdownGriefing.totalTaken = countdownGriefing.totalTaken.plus(
+    entity.amount,
+  )
+  countdownGriefing.save()
+}
+
+export function handleStakeBurned(event: StakeBurned): void {
+  let entity = new StakeBurnedCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.staker = event.params.staker
+  entity.amount = event.params.amount
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let countdownGriefing = new CountdownGriefing(event.address.toHex())
+  countdownGriefing.currentStake = countdownGriefing.currentStake.minus(
+    entity.amount,
+  )
+  countdownGriefing.totalBurned = countdownGriefing.totalBurned.plus(
+    entity.amount,
+  )
+  countdownGriefing.save()
+}
+
+export function handleDeadlineSet(event: DeadlineSet): void {
+  let entity = new DeadlineSetCountdownGriefing(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.deadline = event.params.deadline
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let countdownGriefing = new CountdownGriefing(event.address.toHex())
+  countdownGriefing.deadline = entity.deadline
+  countdownGriefing.save()
+}
---
+++ packages/the-graph/mainnet/v1.2.0/src/Feed.ts
@@ -0,0 +1,122 @@
+import { ByteArray, Bytes } from '@graphprotocol/graph-ts'
+import { InstanceCreated } from '../generated/FeedFactory/FeedFactory'
+import {
+  Initialized,
+  HashSubmitted,
+  OperatorUpdated,
+  MetadataSet,
+} from '../generated/templates/Feed/Feed'
+import { Feed as FeedDataSource } from '../generated/templates'
+import {
+  Feed,
+  InstanceCreatedFeedFactory,
+  InitializedFeed,
+  HashSubmittedFeed,
+  OperatorUpdatedFeed,
+  MetadataSetFeed,
+} from '../generated/schema'
+
+function addQm(a: ByteArray): ByteArray {
+  let out = new Uint8Array(34)
+  out[0] = 0x12
+  out[1] = 0x20
+  for (let i = 0; i < 32; i++) {
+    out[i + 2] = a[i]
+  }
+  return out as ByteArray
+}
+
+export function handleInstanceCreated(event: InstanceCreated): void {
+  let entity = new InstanceCreatedFeedFactory(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.instance = event.params.instance
+  entity.creator = event.params.creator
+  entity.callData = event.params.callData
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let feed = new Feed(event.params.instance.toHex())
+  feed.creator = entity.creator
+  feed.initCallData = entity.callData
+  feed.createdTimestamp = entity.timestamp
+  feed.save()
+
+  FeedDataSource.create(event.params.instance)
+}
+
+export function handleInitialized(event: Initialized): void {
+  let entity = new InitializedFeed(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.operator = event.params.operator
+  entity.proofHash = event.params.proofHash
+  entity.metadata = event.params.metadata
+  entity.metadataB58 = event.params.metadata.toBase58()
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let feed = new Feed(event.address.toHex())
+  feed.initMetadata = entity.metadata
+  feed.initMetadataB58 = entity.metadataB58
+  feed.hashes = []
+  feed.save()
+}
+
+export function handleHashSubmitted(event: HashSubmitted): void {
+  let entity = new HashSubmittedFeed(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.hash = event.params.hash
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let feed = new Feed(event.address.toHex())
+  let hashes = feed.hashes
+  hashes.push(addQm(entity.hash) as Bytes)
+  feed.hashes = hashes
+  feed.save()
+}
+
+export function handleOperatorUpdated(event: OperatorUpdated): void {
+  let entity = new OperatorUpdatedFeed(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.operator = event.params.operator
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let feed = new Feed(event.address.toHex())
+  feed.operator = entity.operator
+  feed.save()
+}
+
+export function handleMetadataSet(event: MetadataSet): void {
+  let entity = new MetadataSetFeed(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  )
+  entity.metadata = event.params.metadata
+  entity.metadataB58 = event.params.metadata.toBase58()
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.save()
+
+  let feed = new Feed(event.address.toHex())
+  feed.metadata = entity.metadata
+  feed.metadataB58 = entity.metadataB58
+  feed.save()
+}
