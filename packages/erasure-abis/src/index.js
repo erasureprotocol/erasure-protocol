5b3a3e424a7bf137a2b32e1fadf23228a9fbb7a6
propulsor <propulsor.ichimoku@gmail.com>
2020-01-17 19:54:39 -0700

    Added v1, v2, v3 abis for @erasure/abis

--- packages/erasure-abis/src/index.js
+++ packages/erasure-abis/src/index.js
@@ -1,3 +1,5 @@
 const ErasureV1 = require("./v1.1.0")
 const ErasureV2 = require("./v1.2.0")
-module.exports={ErasureV1, ErasureV2}
+const ErasureV3 = require("./v1.3.0")
+
+module.exports={ErasureV1, ErasureV2, ErasureV3}
